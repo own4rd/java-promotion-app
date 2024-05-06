88a0573360ddb64e1a0f3a1e97174787e7ec6482
Wagner <wagnerpalmeira.ti@gmail.com>
2024-05-06 10:41:49 -0300

    Adding mockito tests

---
+++ src/test/java/br/com/lowlevel/promotion_app/services/PersonServiceTest.java
@@ -0,0 +1,37 @@
+package br.com.lowlevel.promotion_app.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void createV2() {
+    }
+}
